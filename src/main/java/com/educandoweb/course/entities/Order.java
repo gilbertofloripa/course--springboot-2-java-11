7caeb057064fa5bcafeb09212cb0e8657a5528b9
gilbertofloripa <gilberto_floripa@hotmail.co>
2020-12-03 11:31:12 -0600

    Subtotal & Total methods

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -94,6 +94,14 @@ public class Order implements Serializable{
 	public Set<OrderItem> getItems(){
 		return items;
 	}
+
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItem x : items) {
+			sum+= x.getSubTotal();
+		}
+		return sum;
+	}
 	
 	public Payment getPayment() {
 		return payment;
@@ -127,7 +135,5 @@ public class Order implements Serializable{
 			return false;
 		return true;
 	}
-
-
 	
 }
